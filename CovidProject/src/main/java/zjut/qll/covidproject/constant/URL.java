eb810d2cbbdba06d9f0ff0fd83e14120beefb0a1
Qll <1009608293@qq.com>
2022-03-28 16:55:04 +0800

    删除

--- CovidProject/src/main/java/zjut/qll/covidproject/constant/URL.java
+++
@@ -1,9 +0,0 @@
-package zjut.qll.covidproject.constant;
-
-public interface URL {
-    String NetEase = "https://c.m.163.com/ug/api/wuhan/app/data/list-total";
-    interface Tencent {
-        String CURRENT = "https://view.inews.qq.com/g2/getOnsInfo?name=disease_h5";
-        String HISTORY = "https://api.inews.qq.com/newsqa/v1/query/inner/publish/modules/list?modules=chinaDayList,chinaDayAddList,nowConfirmStatis,provinceCompare";
-    };
-}
